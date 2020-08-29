65b4de06b8f3880b25a845b0bfcdbf38ffd30938
Michael Phelan <mike.g.phelan@gmail.com>
2020-08-29 13:14:06 -0400

    use integerinput in dictionaryentryfullview

---
+++ Count/CommonUI/IntegerInput.swift
@@ -0,0 +1,72 @@
+//
+//  IntegerInput.swift
+//  Count
+//
+//  Created by Michael Phelan on 8/15/20.
+//  Copyright © 2020 MichaelPhelan. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+class IntegerInputVM : ObservableObject {
+    
+    private var ignoreNumericSet = false
+    private var ignoreStringSet = false
+
+    @Published var stringRepresentation = "" {
+        didSet{
+            if(!ignoreStringSet){
+                self.ignoreNumericSet = true
+                self.numericRepresentation = Int(stringRepresentation) ?? nil
+                self.ignoreNumericSet = false
+            }
+        }
+    }
+
+    @Published var numericRepresentation : Int? = nil {
+        didSet{
+            if(!ignoreNumericSet){
+                self.ignoreStringSet = true
+                self.stringRepresentation = numericRepresentation != nil ? numericRepresentation!.description : ""
+                self.ignoreStringSet = false
+            }
+        }
+    }
+    
+    init(_ numericRepresentation: Int?){
+        self.numericRepresentation = numericRepresentation
+    }
+}
+
+struct IntegerInput:View{
+    @Binding var value: Int?
+    @ObservedObject var vm : IntegerInputVM
+    var onFinishedEditing: (() -> Void)?
+    var label: String
+    
+    init(label: String, value: Binding<Int?>, onFinishedEditing: (() -> Void)? = nil) {
+        self._value = value
+        self.onFinishedEditing = onFinishedEditing
+        self.label = label
+        self._vm = ObservedObject(wrappedValue: IntegerInputVM(value.wrappedValue))
+    }
+    
+    var body: some View{
+        return HStack{
+            Text("\(label): ")
+            TextField("", text: self.$vm.stringRepresentation, onEditingChanged: {x in self.updateBinding(x)})
+                .keyboardType(.numberPad)
+            Button(action: {self.hideKeyboard()}){
+                Image(systemName: "checkmark")
+            }
+        }
+    }
+    
+    private func updateBinding(_ editingChangedBool:Bool){
+        if(!editingChangedBool){
+            self.value = self.vm.numericRepresentation
+            if(self.onFinishedEditing != nil) {self.onFinishedEditing!()}
+        }
+    }
+}
--- Count/Dictionary/DictionaryEntryFullView.swift
+++ Count/Dictionary/DictionaryEntryFullView.swift
@@ -14,20 +14,20 @@ struct DictionaryEntryFullView : View {
     
     @State private var name = ""
     @State private var definition = ""
-    @State private var servingSize = ""
+    @State private var servingSize:Int? = nil
     @State private var servingUnit : Units? = nil
-    @State private var calories = ""
-    @State private var protien = ""
+    @State private var calories:Int? =  nil
+    @State private var protien:Int? =  nil
     
     private var entryToEdit : DictionaryEntry? = nil
     
     init(_ entry: DictionaryEntry) {
         _name = State(initialValue: entry.name!)
         _definition = State(initialValue: entry.definition!)
-        _servingSize = entry.servingSize == nil ? State(initialValue: "") : State(initialValue: entry.servingSize!.description)
+        _servingSize = entry.servingSize == nil ? State(initialValue: nil) : State(initialValue: (entry.servingSize as! Int))
         _servingUnit = entry.servingSize == nil ? State(initialValue: nil) : State(initialValue: Units(rawValue: entry.servingUnit as! Int) ?? Units.Gram)
-        _calories = State(initialValue: entry.calories!.description)
-        _protien = State(initialValue: entry.protien!.description)
+        _calories = entry.calories == nil ? State(initialValue: nil) : State(initialValue: (entry.calories as! Int))
+        _protien = entry.protien == nil ? State(initialValue: nil) : State(initialValue: (entry.protien as! Int))
         entryToEdit = entry
     }
     
@@ -38,24 +38,24 @@ struct DictionaryEntryFullView : View {
                 TextField("Name", text: self.$name)
                 MultilineTextField("Description", text:self.$definition )
                 HStack{
-                    TextField("Serving Size", text: self.$servingSize)
+                    IntegerInput(label:"Protion", value: self.$servingSize)
                     Picker(selection: $servingUnit, label: Text("Unit")) {
                         ForEach(Units.allCases, id: \.self) { unit in
                             Text(unit.abbreviation).tag(unit as Units?)
                         }
                     }
                 }
-                TextField("Calories", text: self.$calories)
-                TextField("Protien", text: self.$protien)
+                IntegerInput(label:"Calories", value: self.$calories)
+                IntegerInput(label:"Protien", value: self.$protien)
                 Button(action : {
                     let dictionaryEntry = self.entryToEdit ?? DictionaryEntry(context: self.managedObjectContext)
                     
                     dictionaryEntry.name = self.name
                     dictionaryEntry.definition = self.definition
-                    dictionaryEntry.servingSize = self.servingSize.isEmpty ? nil : NSNumber(value: Int(self.servingSize) ?? 0)
-                    dictionaryEntry.servingUnit = self.servingSize.isEmpty ? nil : NSNumber(value: self.servingUnit!.rawValue)
-                    dictionaryEntry.calories = NSNumber(value: Int(self.calories) ?? 0)
-                    dictionaryEntry.protien = NSNumber(value: Int(self.protien) ?? 0)
+                    dictionaryEntry.servingSize = self.servingSize == nil ? nil : NSNumber(value: self.servingSize ?? 0)
+                    dictionaryEntry.servingUnit = self.servingSize == nil ? nil : NSNumber(value: self.servingUnit!.rawValue)
+                    dictionaryEntry.calories = NSNumber(value: self.calories ?? 0)
+                    dictionaryEntry.protien = NSNumber(value: self.protien ?? 0)
                     
                     do{
                         try self.managedObjectContext.save()
@@ -65,10 +65,10 @@ struct DictionaryEntryFullView : View {
                     
                     self.name = ""
                     self.definition = ""
-                    self.servingUnit = Units.Gram
-                    self.servingSize = ""
-                    self.calories = ""
-                    self.protien = ""
+                    self.servingUnit = nil
+                    self.servingSize = nil
+                    self.calories = nil
+                    self.protien = nil
                     
                     self.presentationMode.wrappedValue.dismiss()
                 }){
