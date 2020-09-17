77b68e6c2f8d180b60af395b3bd33dbeb7175329
Michael Phelan <mike.g.phelan@gmail.com>
2020-09-17 19:20:47 -0400

    put macros on second row and right aligned them

--- Count/Log/LogEntrySimpleView.swift
+++ Count/Log/LogEntrySimpleView.swift
@@ -15,28 +15,34 @@ struct LogEntrySimpleView: View {
     var logEntry:LogEntry
     
     var body: some View {
-        HStack{
-            VStack(alignment: .leading){
-                HStack(){
-                    Image(systemName: vm.isSelected(objectId: self.logEntry.objectID) ? "checkmark.square": "square")
-                    Text(self.logEntry.name ?? "")
-                        .font(.headline)
-                        .lineLimit(1)
-                        .truncationMode(.tail)
-                    Spacer()
-                    Text("Calories: \((self.logEntry.calories ?? 0) as Double, specifier: "%.0f")")
-                        .font(.caption)
-                    Text("Protien: \((self.logEntry.protien ?? 0) as Double, specifier: "%.0f")")
-                        .font(.caption)
-                }.onTapGesture {
-                    let isChecked = self.vm.isSelected(objectId: self.logEntry.objectID)
-                    if(!isChecked){
-                        self.vm.selelctEntry(objectId: self.logEntry.objectID)
-                    }
-                    else if(isChecked) {
-                        self.vm.unselectEntry(objectId: self.logEntry.objectID)
-                    }
-                }
+        VStack(alignment: .leading){
+            HStack(){
+                Image(systemName: vm.isSelected(objectId: self.logEntry.objectID) ? "checkmark.square": "square")
+                Text(self.logEntry.name ?? "")
+                    .font(.headline)
+                    .lineLimit(1)
+                    .truncationMode(.tail)
+            }
+            HStack{
+                Spacer()
+                Text("Calories: \((self.logEntry.calories ?? 0) as Double, specifier: "%.0f")")
+                    .font(.caption)
+                Text("Protien: \((self.logEntry.protien ?? 0) as Double, specifier: "%.0f")")
+                    .font(.caption)
+                Text("Fat: \((0) as Double, specifier: "%.0f")")
+                    .font(.caption)
+                Text("Sugar: \((0) as Double, specifier: "%.0f")")
+                    .font(.caption)
+            }
+        }
+        .contentShape(Rectangle())
+        .onTapGesture {
+            let isChecked = self.vm.isSelected(objectId: self.logEntry.objectID)
+            if(!isChecked){
+                self.vm.selelctEntry(objectId: self.logEntry.objectID)
+            }
+            else if(isChecked) {
+                self.vm.unselectEntry(objectId: self.logEntry.objectID)
             }
         }
     }
